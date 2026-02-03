2ff5f3dac39bb999d5e6bdb9a6a9d5c2c8689783
schmic <147484760+schmic0@users.noreply.github.com>
2026-02-03 15:59:05 +0000

    added helmet function (now working?)

--- myApp.js
+++ myApp.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const app = express();
 const helmet = require("helmet");
-
-module.exports = app;
 const api = require("./server.js");
+
+const app = express();
+
+app.use(express.json());
+
 app.use(express.static("public"));
+
 app.use(helmet.hidePoweredBy());
+
 app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
+
+module.exports = app;
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
