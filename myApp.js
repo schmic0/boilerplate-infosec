3d8d1f343f16e9f50e7bea551090e73f78f5b240
schmic <147484760+schmic0@users.noreply.github.com>
2026-02-04 20:17:50 +0000

    cleaned up some lines of code

--- myApp.js
+++ myApp.js
@@ -1,20 +1,29 @@
 const express = require("express");
-const app = express();
 const helmet = require("helmet");
 
+const app = express();
+
+// Helmet security headers
 app.use(helmet.hidePoweredBy());
-
 app.use(helmet.frameguard({ action: "deny" }));
+// app.use(helmet.hsts()); // only enable if you actually want HSTS
 
-module.exports = app;
-const api = require("./server.js");
+// Static files
 app.use(express.static("public"));
-app.disable("strict-transport-security");
+
+// API routes
+const api = require("./api");
 app.use("/_api", api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+
+// Main route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
-let port = process.env.PORT || 3000;
+
+// Start server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
+module.exports = app;
