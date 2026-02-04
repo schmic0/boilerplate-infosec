8cf44121f5e4851beeebd8d49a4efc6280c17f10
schmic <147484760+schmic0@users.noreply.github.com>
2026-02-04 20:19:55 +0000

    fixed code

--- myApp.js
+++ myApp.js
@@ -1,29 +1,20 @@
 const express = require("express");
+const app = express();
 const helmet = require("helmet");
 
-const app = express();
-
-// Helmet security headers
 app.use(helmet.hidePoweredBy());
+
 app.use(helmet.frameguard({ action: "deny" }));
-// app.use(helmet.hsts()); // only enable if you actually want HSTS
 
-// Static files
+module.exports = app;
+const api = require("./server.js");
 app.use(express.static("public"));
-
-// API routes
-const api = require("./api");
+app.disable("strict-transport-security");
 app.use("/_api", api);
-
-// Main route
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
-
-// Start server
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-module.exports = app;
